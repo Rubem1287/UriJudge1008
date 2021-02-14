bc003690a518b2176ede9d3dcc19f967acf8d12d
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:28:28 -0300

    Add files via upload

---
+++ Uri 1008/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c, salario;
+
+            Console.WriteLine();
+            Console.Write("Digite o número do funcionário : ");
+            a = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+            Console.Write("Digite o número do horas trabalhadas : ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o valor de cada hora trabalhada : ");
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = b * c;
+
+            Console.WriteLine();
+            Console.Write("Número = "+a);
+           
+
+            Console.WriteLine();
+            Console.Write("Salário = R$ "+salario.ToString("F2", CultureInfo.InvariantCulture));
+           
+
+        }
+    }
+}
